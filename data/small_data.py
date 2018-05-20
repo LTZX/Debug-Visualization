b5b740869b684f08e133ea460aaef395434ed79c
LTZX <yzxu@ucdavis.edu>
2018-05-19 18:32:24 -0700

    data update

--- data/small_data.py
+++ data/small_data.py
@@ -13,25 +13,25 @@ methods = [{'name': 'main', 'class': 'api', 'file': 'apt.java',
             'related': [], 'relations': []},
            {'name': 'main', 'class': 'database', 'file': 'database.java',
             'directory': 'Java/Database', 'lines': 5, 'bugs': [4], 'MID': '2',
-            'related': [0,3], 'relations': [3,4]}, 
+            'related': ['0','3'], 'relations': ['3','4']}, 
            {'name': 'calculateArea', 'class': 'sensor', 'file': 'sensor.java',
             'directory': 'Java/Sensor', 'lines': 3, 'bugs': [], 'MID': '3',
-            'related': [6], 'relations': [0]},
+            'related': ['6'], 'relations': ['0']},
            {'name': 'calculateArea', 'class': 'sensor', 'file': 'sensor.java',
             'directory': 'Java/Sensor', 'lines': 3, 'bugs': [], 'MID': '4',
-            'related': [6], 'relations': [1]},
+            'related': ['6'], 'relations': ['1']},
            {'name': 'calculateArea', 'class': 'sensor', 'file': 'sensor.java',
             'directory': 'Java/Sensor', 'lines': 4, 'bugs': [], 'MID': '5',
-            'related': [6], 'relations': [2]},
+            'related': ['6'], 'relations': ['2']},
            {'name': 'main', 'class': 'sensor', 'file': 'sensor.java',
             'directory': 'Java/Sensor', 'lines': 18, 'bugs': [2,12], 'MID': '6',
             'related': [], 'relations': []}]
-relations = {'0': {'from':6, 'to':3},
-             '1': {'from':6, 'to':4},
-             '2': {'from':6, 'to':5},
-             '3': {'from':3, 'to':2},
-             '4': {'from':0, 'to':2},
-             '5': {'from':1, 'to':0}}
+relations = {'0': {'from':'6', 'to':'3'},
+             '1': {'from':'6', 'to':'4'},
+             '2': {'from':'6', 'to':'5'},
+             '3': {'from':'3', 'to':'2'},
+             '4': {'from':'0', 'to':'2'},
+             '5': {'from':'1', 'to':'0'}}
 git = { '0': [
             {'author':'AAA', 'time':'2018-05-08 12:32:32', 'comment':'zxcas','GID':'1b'},
             {'author':'CCC', 'time':'2018-05-12 12:32:32', 'comment':'dasfe','GID':'3c'},
@@ -93,14 +93,14 @@ for each in methods:
     groupby['class'][each['class']].append(each['MID'])
     groupby['file'][each['file']].append(each['MID'])
     groupby['directory'][each['directory']].append(each['MID'])
-    if len(git[each['MID']]) == 0:
-        continue
-    else:
-        for commit in git[each['MID']]:
-            if commit['author'] not in groupby['author']:
-                groupby['author'] = []
-            groupby['author'].append(each['MID'])
 
+for each in git:
+    if len(git[each]) != 0:
+        for commit in git[each]:
+            if commit['author'] not in groupby['author']:
+                groupby['author'][commit['author']] = []
+            if each not in groupby['author'][commit['author']]:
+                groupby['author'][commit['author']].append(each)
 
 data = {'filedata': filedata, 'force': force, 'bugs': bugs, 'groupby': groupby, 'git':git}
 with open('data.json', 'w') as outfile:
